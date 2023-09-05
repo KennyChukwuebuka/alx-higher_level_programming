13d60781be70d496f2c35728f03dcbe6694a5b6d
kennyChukwuebuka <kenkinado@gmail>
2023-09-05 21:35:33 +0100

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for first_digit_number in range(10):
+for first_digit_number in range(9):
     for second_digit_number in range(first_digit_number + 1, 10):
-        print("{:02d}, {:02d}".format(first_digit_number, second_digit_number),
-              end=", " if second_digit_number < 9 else "\n")
+        print("{:d}{:d}".format(first_digit_number, second_digit_number),
+              end=", " if first_digit_number != 8
+              or second_digit_number != 9 else "\n")
