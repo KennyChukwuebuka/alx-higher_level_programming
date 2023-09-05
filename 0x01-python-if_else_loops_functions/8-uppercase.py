0268cf67eb85713cdadcd3658b6f720ffac34af2
kennyChukwuebuka <kenkinado@gmail>
2023-09-05 22:10:07 +0100

    upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+    print()
