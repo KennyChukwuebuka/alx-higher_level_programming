2c8fab5bb3d4f2d85b7cf99bf2da988f74b107db
kennyChukwuebuka <kenkinado@gmail>
2023-09-07 17:01:37 +0100

    My tool box

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
 
-    result_add = calculator_1.add(10, 5)
-    result_sub = calculator_1.sub(10, 5)
-    result_mul = calculator_1.mul(10, 5)
-    result_div = calculator_1.div(10, 5)
+    result_add = add(10, 5)
+    result_sub = sub(10, 5)
+    result_mul = mul(10, 5)
+    result_div = div(10, 5)
 
     print('{} + {} = {}'.format(a, b, result_add))
     print('{} - {} = {}'.format(a, b, result_sub))
