7090ecf8253bb220c8708b51b8ca399292ca6eb1
kennyChukwuebuka <kenkinado@gmail>
2023-09-07 16:46:10 +0100

    My tool box

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     from calculator_1 import mul
     from calculator_1 import div
 
-    result = add(10, 5)
-    result = sub(10, 5)
-    result = mul(10, 5)
-    result = div(10, 5)
+    result_add = add(10, 5)
+    result_sub = sub(10, 5)
+    result_mul = mul(10, 5)
+    result_div = div(10, 5)
 
-    print('{} + {} = {}'.format(a, b, result))
-    print('{} - {} = {}'.format(a, b, result))
-    print('{} * {} = {}'.format(a, b, result))
-    print('{} / {} = {}'.format(a, b, result))
+    print('{} + {} = {}'.format(a, b, result_add))
+    print('{} - {} = {}'.format(a, b, result_sub))
+    print('{} * {} = {}'.format(a, b, result_mul))
+    print('{} / {} = {}'.format(a, b, result_div))
