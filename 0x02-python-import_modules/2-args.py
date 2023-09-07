814e17c9a84d161e81430402292cb35642cd071d
kennyChukwuebuka <kenkinado@gmail>
2023-09-07 21:25:01 +0100

    Build a dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    # get the c-line argument
+    arguments = sys.argv[1:]
+
+    num_of_arguments = len(arguments)
+    # print number of arguments
+    if num_of_arguments == 0:
+        print("{} arguments.".format(num_of_arguments))
+    else:
+        print("{} arguments:".format(num_of_arguments))
+    # get the list of all arguments
+    if num_of_arguments > 0:
+        for i, num_of_arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, num_of_arg))
