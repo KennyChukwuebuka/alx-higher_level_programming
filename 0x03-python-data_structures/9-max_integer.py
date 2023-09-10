b256f5c079d8846ebb6139ca0eb12f2003cf259d
kennyChukwuebuka <kenkinado@gmail>
2023-09-10 21:03:15 +0100

    find biggest number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+
+    max_num_of_ele = my_list[0]
+
+    for number in my_list:
+        if number > max_num_of_ele:
+            max_num_of_ele = number
+    return max_num_of_ele
