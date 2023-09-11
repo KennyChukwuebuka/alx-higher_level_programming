e7c5126700723eedc39bcf6fe5ea8fb670d6e495
kennyChukwuebuka <kenkinado@gmail>
2023-09-11 20:48:12 +0100

    function that delete index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
+    """My delete function
+
+    Args:
+        my_list: first arg
+        idx: second arg
+
+        Returns:
+            The copy of new list value
+    """
     # check if the list is empty or if idx is out of range
     if not my_list or idx < 0 or idx >= len(my_list):
         return my_list
