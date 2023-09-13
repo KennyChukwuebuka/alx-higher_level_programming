395c53124d447fe144bfd3460741b1c7bc3c313a
kennyChukwuebuka <kenkinado@gmail>
2023-09-13 18:04:00 +0100

    updates dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """update_dictionary
+
+    Args:
+        a_dictionary:
+        key:
+        value:
+
+    Return:
+        Updated dictionaries
+    """
+    a_dictionary[key] = value
+    return a_dictionary
