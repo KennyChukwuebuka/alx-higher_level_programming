52e52b36e91c7de322802c0b1786985df40b6069
kennyChukwuebuka <kenkinado@gmail>
2023-09-13 05:23:18 +0100

    CPyton

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,22 +8,27 @@
 
 #include <Python.h>
 
-void print_python_list_info(PyObject *p) {
-    if (!PyList_Check(p)) {
-        printf("Invalid argument: Not a Python list.\n");
-        return;
-    }
+void print_python_list_info(PyObject *p)
+{
+	if (!PyList_Check(p))
+	{
+		printf("Invalid argument: Not a Python list.\n");
+		return;
+	}
 
-    Py_ssize_t size = PyList_Size(p);
-    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", allocated);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
 
-    if (size > 0) {
-        PyObject *last_element = PyList_GetItem(p, size - 1);
-        printf("[*] Element %ld: %s\n", size - 1, Py_TYPE(last_element)->tp_name);
-    }
+	if (size > 0)
+	{
+		PyObject *last_element = PyList_GetItem(p, size - 1);
 
-    printf("[*] Element %ld: %s\n", p->ob_refcnt, Py_TYPE(p)->tp_name);
+		printf("[*] Element %ld: %s\n", size - 1,
+				Py_TYPE(last_element)->tp_name);
+	}
+
+	printf("[*] Element %ld: %s\n", p->ob_refcnt, Py_TYPE(p)->tp_name);
 }
