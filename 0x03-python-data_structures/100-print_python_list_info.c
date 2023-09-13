5388db86f46b8ad8feb3df45d669e6439a1ff309
kennyChukwuebuka <kenkinado@gmail>
2023-09-13 04:55:01 +0100

    CPython Test

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+
+    if (!PyList_Check(p)) {
+        printf("Invalid argument: Not a Python list.\n");
+        return;
+    }
+
+    Py_ssize_t size = PyList_Size(p);
+
+
+    Py_ssize_t ref_count = p->ob_refcnt;
+
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+    printf("[*] Element %ld: %s\n", size, Py_TYPE(PyList_GetItem(p, size-1))->tp_name);
+    printf("[*] Element %ld: %s\n", ref_count, Py_TYPE(p)->tp_name);
+}
