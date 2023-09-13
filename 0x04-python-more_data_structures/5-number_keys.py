691271640e4e0410c7f9321378c3a067d67e51b1
kennyChukwuebuka <kenkinado@gmail>
2023-09-13 17:27:16 +0100

    function that return key element in dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """number_keys
+
+    Args:
+        a_dictionary:
+
+    Return:
+        The number of key in a dictionary
+    """
+    num_of_key = len(a_dictionary.keys())
+
+    return num_of_key
