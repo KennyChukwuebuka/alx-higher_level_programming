8836380f39263182c4dc965c512a28fd9e0a7aff
kennyChukwuebuka <kenkinado@gmail>
2023-09-13 05:06:35 +0100

    CPython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,8 +13,13 @@ void print_python_list_info(PyObject *p) {
 
     Py_ssize_t ref_count = p->ob_refcnt;
 
+
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-    printf("[*] Element %ld: %s\n", size, Py_TYPE(PyList_GetItem(p, size-1))->tp_name);
+
+    if (size > 0) {
+        printf("[*] Element %ld: %s\n", size - 1, Py_TYPE(PyList_GetItem(p, size - 1))->tp_name);
+    }
+
     printf("[*] Element %ld: %s\n", ref_count, Py_TYPE(p)->tp_name);
 }
