d380b2f72300513807f594f4b1ecb961e78c4d01
kennyChukwuebuka <kenkinado@gmail>
2023-09-15 20:48:26 +0000

    cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -12,8 +12,8 @@ void print_python_list(PyObject *p)
 	size = ((PyVarObject *)p)->ob_size;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
 	for (i = 0; i < size; i++) {
 		PyObject *item = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
@@ -30,6 +30,7 @@ void print_python_bytes(PyObject *p)
 	char *data;
 
 	printf("[.] bytes object info\n");
+
 	if (!PyBytes_Check(p)) {
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
@@ -42,9 +43,12 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", data);
 
 	printf("  first %ld bytes: ", (size < 10) ? size : 10);
-	for (i = 0; i < size && i < 10; i++) {
+
+	for (i = 0; i < size; i++) {
 		printf("%02hhx", data[i]);
-		if (i < 9)
+		if (i == 9)
+			break;
+		if (i < size - 1)
 			printf(" ");
 	}
 	printf("\n");
