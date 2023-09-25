01e7fbcc4d9e4d4247aca81a8ef2cadff07aa723
kennyChukwuebuka <kenkinado@gmail>
2023-09-25 20:01:36 +0100

    list of div

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,34 +1,34 @@
-#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    """list_division
-
-        Args:
-            my_list_1:  integer or string
-            my_list_2:  integer or string
-            list_length:    length of both
-
-        Return:
-            a new list with all division
-    """
-    ret = []
+    result = []
     try:
-        for item in range(list_length):
-            if item >= len(my_list_1) or item >= len(my_list_2):
+        for i in range(list_length):
+            # Check if both lists are too short
+            if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError("out of range")
+
+            # Attempt the division
             try:
-                alist = float(my_list_1[item])
-                blist = float(my_list_2[item])
-                if blist == 0:
+                a = float(my_list_1[i])
+                b = float(my_list_2[i])
+                if b == 0:
                     raise ZeroDivisionError("division by 0")
-                divided_result = alist / blist
-                ret.append(divided_result)
+                division_result = a / b
+                result.append(division_result)
             except ValueError:
-                ret.append(0)
+                result.append(0)
                 print("wrong type")
             except ZeroDivisionError:
-                ret.append(0)
+                result.append(0)
                 print("division by 0")
     except IndexError:
         print("out of range")
     finally:
-        return ret
+        return result
+
+# Example usage:
+my_list_1 = [10, 20, 30]
+my_list_2 = [2, 0, "40"]
+list_length = 5
+
+result = list_division(my_list_1, my_list_2, list_length)
+print(result)
