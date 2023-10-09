0f33d8b2beb6d931dab4394dba3b71d0df9e0842
kennyChukwuebuka <kenkinado@gmail>
2023-10-09 19:42:18 +0100

    add attr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,12 @@
 
 
 def add_attribute(obj, attr_name, attr_value):
+    """Define add_attribute
+    Args:
+        obj
+        attr_name
+        attr_value
+    """
     if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
