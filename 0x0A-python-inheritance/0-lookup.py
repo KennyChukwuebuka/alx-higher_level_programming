39d00f6904acff7cbaa545cb58d9b2b64fd19260
kennyChukwuebuka <kenkinado@gmail>
2023-10-09 15:10:20 +0100

    hot fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,14 +7,4 @@ def lookup(obj):
     Args:
         obj
     """
-    # Use dir() to get the list of attributes and methods
-    attributes_and_methods = dir(obj)
-    return attributes_and_methods
-
-# Example usage:
-
-
-my_list = [1, 2, 3]
-result = lookup(my_list)
-for item in result:
-    print(item)
+    return dir(obj)
