2e296e0680e296b46c4c8ffee153c36df6abdcf7
kennyChukwuebuka <kenkinado@gmail>
2023-10-10 21:06:30 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" function that returns the dictionary description
+with simple data structure (list, dictionary, string, 
+integer and boolean) for JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """  function that returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object
+    """
+    dic = {}
+    if hasattr(obj, "__dict__"):
+        dic = obj.__dict__.copy()
+    return dic
