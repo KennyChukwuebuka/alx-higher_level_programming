740e280caabf90a9c29c32f57f10e0c787cc03fb
kennyChukwuebuka <kenkinado@gmail>
2023-10-11 19:26:46 +0100

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
-""""""
+"""Define append after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Define append_after
+    Args:
+        filename
+        search_string
+        new_string
+    """
+    read = []
+    with open(filename, "r", encoding="utf-8") as f:
+        read = f.readlines()
+        index = 0
+
+        while index < len(read):
+            if search_string in read[index]:
+                read[index:index + 1] = [read[index], new_string]
