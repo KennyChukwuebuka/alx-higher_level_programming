d6e1aacd8e2d2691dcffea36886032edde50e270
kennyChukwuebuka <kenkinado@gmail>
2023-10-11 19:40:12 +0100

    fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Define append after"""
-
+'''append_after'''
 
 def append_after(filename="", search_string="", new_string=""):
     """Define append_after
     Args:
         filename
         search_string
-        new_string
     """
     read = []
     with open(filename, "r", encoding="utf-8") as f:
         read = f.readlines()
-        index = 0
+    
+    for index, line in enumerate(read):
+        if search_string in line:
+            read.insert(index + 1, new_string)
 
-        while index < len(read):
-            if search_string in read[index]:
-                read[index:index + 1] = [read[index], new_string]
+    with open(filename, "w", encoding="utf-8") as file:
+        file.writelines(read)
