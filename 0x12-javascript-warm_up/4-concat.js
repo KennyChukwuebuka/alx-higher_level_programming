32353fc66ba952acdc5c8a6b3dda996483d8108a
kennyChukwuebuka <kenkinado@gmail>
2023-12-11 20:34:43 +0100

    fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@
 const args = process.argv.slice(2);
 
 if (args.length >= 1) {
-    const firstArg = args[0];
-    const secondArg = args.length > 1 ? args[1] : "undefined";
-    console.log(`${firstArg} is ${secondArg}`);
+  const firstArg = args[0];
+  const secondArg = args.length > 1 ? args[1] : 'undefined';
+  console.log(`${firstArg} is ${secondArg}`);
 } else {
-    console.log(`undefined is undefined`);
+  console.log('undefined is undefined');
 }
