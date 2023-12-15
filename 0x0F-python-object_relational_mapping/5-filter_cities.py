8c527c0d2f52afceb7f6d1878237b4fb9eac9734
kennyChukwuebuka <kenkinado@gmail>
2023-12-16 00:09:22 +0100

    script that takes names of state as argument

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+Results must be sorted in ascending order by cities.id
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities " +
+                "INNER JOIN states ON cities.state_id = states.id " +
+                "WHERE states.name = %s ORDER BY cities.id", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
