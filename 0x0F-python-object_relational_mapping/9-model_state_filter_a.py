1b3d275257e36aa8c66602344cc01624bb78e9f3
kennyChukwuebuka <kenkinado@gmail>
2023-12-16 20:13:15 +0100

    hot fix

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,8 @@ You must use the module SQLAlchemy
 
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
