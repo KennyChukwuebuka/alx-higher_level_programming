7e74c92136feb7783be204658b176bce88260621
kennyChukwuebuka <kenkinado@gmail>
2024-01-05 13:33:55 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL to the end
-[ $# -eq 0 ] && echo "Usage: $0 <URL>" && exit 1; curl -s "$1" | grep -o "Route 2"
+[ $# -eq 0 ] && echo "Usage: $0 <URL>" && exit 1; curl -s "$1" | grep -oP 'Route\s+\d+'
