feffd6bacdace1d9992244638e3eb5161acde0ce
kennyChukwuebuka <kenkinado@gmail>
2024-01-05 20:25:19 +0100

    task 5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that takes in a URL, sends a POST request to the passed URL
+curl -s -d "email=curl -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
