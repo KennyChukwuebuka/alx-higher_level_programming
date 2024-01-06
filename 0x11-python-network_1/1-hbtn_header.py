d88afd5d87d2dcad4b96167e623a8570ba99036c
kennyChukwuebuka <kenkinado@gmail>
2024-01-06 22:28:32 +0100

    script that takes URL and send request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""write a script that takes in a URL
+   - sends a request to the URL
+   -and displays the value of the X-Request-Id
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+    else:
+        url = sys.argv[1]
+        try:
+            with urllib.request.urlopen(url) as response:
+                request_id = response.getheader('X-Request-Id')
+                if request_id:
+                    print(f"{request_id}")
+                else:
+                    print("X-Request-Id not found")
+        except urllib.error.HTTPError as e:
+            print(f"HTTP Error: {e.code} - {e.reason}")
+        except urllib.error.URLError as e:
+            print(f"URL Error: {e.reason}")
