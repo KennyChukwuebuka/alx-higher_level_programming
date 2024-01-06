314df3023288fc5ca9c28132f2f256cd7564f6d8
kennyChukwuebuka <kenkinado@gmail>
2024-01-06 22:36:50 +0100

    script that takes URL and send request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,14 +11,6 @@ if __name__ == "__main__":
         print("Usage: python script_name.py <URL>")
     else:
         url = sys.argv[1]
-        try:
-            with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
                 request_id = response.getheader('X-Request-Id')
-                if request_id:
-                    print("{}".format(request_id))
-                else:
-                    print("X-Request-Id variable not found.")
-        except urllib.error.HTTPError as e:
-            print("HTTP Error: {} - {}".format(e.code, e.reason))
-        except urllib.error.URLError as e:
-            print("URL Error: {}".format(e.reason))
+                print("{}".format(request_id))
