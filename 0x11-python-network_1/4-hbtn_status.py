89dd29317529bd18aca6fba2d215acca3c2b27d1
kennyChukwuebuka <kenkinado@gmail>
2024-01-07 05:20:59 +0100

    script that fetche url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,6 +10,6 @@ response = requests.get(url)
 
 response.status_code == 200
 
-print("Body response")
+print("Body response:")
 print("\t- type:", type(response.text))
 print("\t- content", response.text)
