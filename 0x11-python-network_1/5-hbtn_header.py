6984da562a7027d4427a4be8e27f21582af9a220
kennyChukwuebuka <kenkinado@gmail>
2024-01-07 05:42:38 +0100

    script that takes url and sends req

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes in a url
+   -sends a request to the url
+   -display value of the variable X-Response-Id
+"""
+
+import requests
+import sys
+
+url = sys.argv[1]
+
+response = requests.get(url)
+
+'X-Request-Id' in response.headers
+
+print("{}".format(response.headers['X-Request-Id']))
