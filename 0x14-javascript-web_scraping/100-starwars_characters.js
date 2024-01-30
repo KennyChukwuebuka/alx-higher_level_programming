bae7d9b9cb696986e78a7cdd69badfe679083376
kennyChukwuebuka <kenkinado@gmail>
2024-01-30 23:36:53 +0100

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,10 +7,16 @@
 
 const request = require('request');
 
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).characters);
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
   }
 });
